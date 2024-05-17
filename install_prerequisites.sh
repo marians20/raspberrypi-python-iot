a1d68830a209ad5b1699c564c3959adc03769125
Marian Spoiala <marian.spoiala@gmail.com>
2024-05-17 04:40:42 +0300

    Introduced DI

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -6,6 +6,7 @@ apt install git -y
 apt install build-essential -y
 pip3 install adafruit-circuitpython-htu21d --break-system-packages
 apt install pigpio python3-pigpio
+pip3 install dependency-injector --break-system-packages
 pip3 install adafruit-circuitpython-dht --break-system-packages
 pip3 install Adafruit_DHT --break-system-packages --install-option="--force-pi"
 systemctl enable pigpiod
